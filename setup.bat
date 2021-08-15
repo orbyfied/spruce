50f40def22d90a2f39b17ca808c4dcf90077ea31
Orbyfied <officialglitchout@gmail.com>
2021-08-15 17:39:11 +0200

    made setup script

---
+++ setup.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: [ CHARACTER REFERENCE ]
+:: ╔╗╚╝║═
+:: ╔╗╚╝║═
+
+echo|set /p="[1;91m"
+echo "[1;91m+----------------------------+"
+echo "[1;91m|Spruce project setup script.|"
+echo "[1;91m+----------------------------+"
+
+echo [33mCreating natives output folder (required to build natives).
+echo|set /p="[1;92m>> [0m"
+mkdir build\natives
+
+echo|set /p="[0m"
+
+echo|set /p="[1;91m"
+echo "[1;91m+-------+"
+echo "[1;91m|[Done!]|"
+echo "[1;91m+-------+"
+
+echo|set /p="[0m"
