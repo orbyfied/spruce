e328d0086ed7d1f9d865d556bec47e63ee941661
orbyfied <72315780+orbyfied@users.noreply.github.com>
2021-11-03 11:14:26 +0100

    Delete .ccls-cache directory

--- .ccls-cache/C@@Users@atomf@Desktop@code@spruce/src@win32@cpp@const.hpp
+++
@@ -1,7 +0,0 @@
-// windows console modes
-#define ENABLE_PROCESSED_OUTPUT            0x0001
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-
-// exception class names
-#define EXCEPTION_ILLEGAL_STATE    "java/lang/IllegalStateException"
-#define EXCEPTION_ILLEGAL_ARGUMENT "java/lang/IllegalArgumentException"
